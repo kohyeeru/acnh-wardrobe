1b779b315b62ce2a067c2d3ee65d0056635e360f
iNekox3 <inekox3@hotmail.com>
2020-05-17 16:37:35 +0800

    Add caching to server

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -3,10 +3,21 @@ const axios = require('axios');
 
 const router = express.Router();
 
+const cache = {};
+
 router.get('/:category', (req, res) => {
   const category = req.params.category;
+
+  if (category in cache) {
+    return res.json(cache[category]);
+  }
+
   axios.get(`${process.env.RESOURCE_API_URL}/${category}`)
-    .then((apiResult) => { res.send(apiResult.data); })
+    .then((apiResult) => {
+      const data = apiResult.data;
+      cache[category] = data;
+      res.json(data);
+    })
     .catch((err) => {
       console.log(err);
       res.send(err);
