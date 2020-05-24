b18d72167aa412ebb61fde09bee0ac42f3797058
iNekox3 <inekox3@hotmail.com>
2020-05-25 01:48:24 +0800

    Add character display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Categories } from './constants/categoryEnums';
+import { Outfit } from './constants/outfitTypes';
 import CategoryNavBar from './components/CategoryNavBar/CategoryNavBar';
+import CharacterDisplay from './components/CharacterDisplay/CharacterDisplay';
 
 const Wrapper = styled.div`
-  margin-top: 24px;
+  padding: 24px;
 `
 
 interface IState {
-  data: any;
+  outfit: Outfit;
 }
 
 class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      outfit: {},
+    }
+  }
+
+  // TODO: amend to require only clothing name after library of clothing is created
+  public setOutfit(category: Categories, clothingName: string, imageURL: string, top: string, left: string) {
+    const { outfit } = this.state;
+    
+    let newOutfit:Outfit = {};
+    newOutfit = Object.assign(newOutfit, outfit);
+    newOutfit[category] = {
+      name: clothingName,
+      imageURL,
+      top,
+      left,
+    }
+
+    this.setState({ outfit: newOutfit });
+  }
+
   public render() {
+    const { outfit } = this.state;
+
     return (
       <Wrapper>
         <CategoryNavBar />
+        <CharacterDisplay
+          outfit={outfit}
+        />
       </Wrapper>
     );
   }
